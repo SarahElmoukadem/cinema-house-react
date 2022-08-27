1cfbf7ac0f56cd5e730a2197788188cf19695a9b
sarahelmoukadem <sara.mokadm@iqraaly.com>
2022-08-27 11:48:44 -0700

    handle pagination

---
+++ src/components/Pagination/CustomPagination.js
@@ -0,0 +1,33 @@
+import { Pagination } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import React from 'react';
+
+const darkTheme = createTheme({
+    palette: {
+        mode: "dark",
+    },
+});
+
+const CustomPagination = ({ setPage, numberOfPages = 10 }) => {
+    const handlePageChange = (page) => {
+        setPage(page);
+        window.scroll(0, 0)
+    }
+    return (
+        <div style={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+            marginTop: 10,
+        }}>
+            <ThemeProvider theme={darkTheme}>
+                <Pagination
+                    count={numberOfPages}
+                    onChange={(e) => handlePageChange(e.target.textContent)} />
+
+            </ThemeProvider>
+        </div>
+    )
+}
+
+export default CustomPagination
