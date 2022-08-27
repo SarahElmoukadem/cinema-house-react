31efdd8582882bf9df55b145eab83cf96d923c3b
sarahelmoukadem <sara.mokadm@iqraaly.com>
2022-08-27 11:47:24 -0700

    handle pages routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import { Container } from '@mui/system';
 import './App.css';
+import Header from './components/Header/Header';
+import MainNav from './components/MainNav/MainNav';
+import Trending from './pages/Trending/Trending';
+
+import { Search } from '@mui/icons-material';
+import { Route, Routes } from "react-router-dom";
+import Movies from './pages/Movies/Movies';
+import Series from './pages/Series/Series';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="app">
+        <Container>
+
+          <Routes>
+            <Route path="/trending" element={<Trending/>} />
+            <Route path="/movies" element={<Movies/>} />
+            <Route path="/series" element={<Series/>} />
+            <Route path="/search" element={<Search/>} />
+          </Routes>
+
+        </Container>
+      </div>
+      <MainNav />
+    </>
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <span className='header'>
+      Cinema House 🎬
+    </span>
+  )
+}
+
+export default Header
